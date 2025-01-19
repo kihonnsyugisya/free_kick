0e3f764a56b703d68e26a51e16debd2c43da5a06
YOSIHIRO\katuo <katuobrothers@gmail.com>
2025-01-19 17:04:39 +0900

    feat: demoシーン作成

--- template/Assets/Scripts/ButtonController.cs
+++ template/Assets/Scripts/ButtonController.cs
@@ -2,21 +2,53 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 using UniRx;
+using GoogleMobileAds.Sample;
+using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
     private const int ButtonThrottleTimeMs = 2000; // ボタンの連続クリック防止時間 (ms)
     [SerializeField] private Button reviewButton; // レビュー機能ボタン
+    [SerializeField] private Button interstitialButton; // インステボタン
+    [SerializeField] private InterstitialAdController interstitialAdController; // 広告管理コントローラ
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         ConfigureReviewButton();
+        ConfigureInterstitialButton();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// インステボタンのクリックイベントを設定。
+    /// </summary>
+    private void ConfigureInterstitialButton()
+    {
+        interstitialButton.OnClickAsObservable()
+            .ThrottleFirst(TimeSpan.FromMilliseconds(ButtonThrottleTimeMs))
+            .TakeUntilDestroy(this)
+            .Subscribe(_ => ShowAd());
+    }
+
+    /// <summary>
+    /// インステ表示
+    /// </summary>
+    private void ShowAd()
+    {
+        if (!interstitialAdController.isSkipAd)
+        {
+            interstitialAdController.ShowAd();
+        }
+        else
+        {
+            // 画面遷移とか
+            //SceneManager.LoadScene("TitleScene");
+        }
     }
 
     /// <summary>
@@ -30,3 +62,7 @@ public class ButtonController : MonoBehaviour
             .Subscribe(_ => StartCoroutine(InAppReviewManager.RequestReview()));
     }
 }
+
+//admobいれたから、ATTとかバナー広告とか出せるでもシーンつくるところから。それができたら実機にビルドしてちゃんとテスト広告出せてるか見る。
+//    できたらGDRPについてどうたいおうするのかもしらべてナレッジに対応策まとめたい
+    
