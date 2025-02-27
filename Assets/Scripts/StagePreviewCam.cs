5c5ac2e31e3273a6a4f5bd76376ee18d34f65418
YOSIHIRO\katuo <katuobrothers@gmail.com>
2025-02-27 23:36:19 +0900

    feat: 実装中

---
+++ Assets/Scripts/StagePreviewCam.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class StagePreviewCam : MonoBehaviour
+{
+    [SerializeField] private CameraSwitcher cameraSwitcher;
+    [SerializeField] private UIController uIController;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    async void Start()
+    {
+        await PlayStageSequence();
+    }
+
+    private async Task PlayStageSequence()
+    {
+        uIController.ShowControllUis(false);
+        cameraSwitcher.SwitchCamera(CameraSwitcher.CameraType.Replay2);
+        await Task.Delay(3200);
+        await Task.Delay(1000);
+        cameraSwitcher.SwitchCamera(CameraSwitcher.CameraType.Kicker);
+        await Task.Delay(900);
+        uIController.ShowControllUis(true);
+        uIController.powerSlider.hideArrowSlider();
+        uIController.ShowRetryButton(false);
+    }
+}
